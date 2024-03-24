37eddee5d7e821c027568982f7dc9602b52ba67a
sammysium <46919720+sammysium@users.noreply.github.com>
2024-03-24 12:15:54 +0200

    changes

---
+++ src/hooks/index.ts
@@ -0,0 +1,5 @@
+export { default as useHandleAnswering} from "./useHandleAnswering";
+export { useMaskInput} from "./useMaskInput";
+export { default as useFieldValidator} from "./useFieldValidator";
+export { useMaskValidation } from "./useMaskValidation";
+export { useRunValidations } from "./useRunValidations";
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export { default as Consumer } from "./Consumer";
 export { default as Builder } from "./Builder"
+export * from "./contexts"
+export * from "./hooks"
+export * from "./utils";
